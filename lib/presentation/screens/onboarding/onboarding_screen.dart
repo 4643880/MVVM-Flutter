67dbad79025d2cce1d3553ba9b01a5f6651aaea9
Aizaz Haider <aizazhaider720@gmail.com>
2024-04-01 12:25:39 +0500

    Added Timer to splash

--- lib/presentation/screens/onboarding/onboarding_screen.dart
+++ lib/presentation/screens/onboarding/onboarding_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_flutter_app/presentation/resources/colors_manager.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+      body: Center(
+        child: Text("Welcome to on baording"),
+      ),
+    );
   }
 }
--- lib/presentation/screens/splash/splash_screen.dart
+++ lib/presentation/screens/splash/splash_screen.dart
@@ -1,9 +1,47 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:mvvm_flutter_app/presentation/resources/assets_manager.dart';
 import 'package:mvvm_flutter_app/presentation/resources/colors_manager.dart';
+import 'package:mvvm_flutter_app/presentation/resources/routes_manager.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  Timer? _timer;
+
+  _startDelay() {
+    _timer = Timer(
+      const Duration(seconds: 2),
+      () => _goNext(),
+    );
+  }
+
+  _goNext() {
+    Navigator.pushReplacementNamed(
+      context,
+      Routes.onBoardingRoute,
+    );
+  }
+
+  @override
+  void initState() {
+    _startDelay();
+    //   FlutterNativeSplash.remove();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
